f26532466e02345ff4dc5bd5e6f80137a8316fc5
Nissa Ferm <nissa.ferm@noaa.gov>
2018-11-30 16:16:46 -0800

    polygon works now, can plot

--- FastrCAT/R/slice_plot_fc.R
+++ FastrCAT/R/slice_plot_fc.R
@@ -34,18 +34,20 @@ idw_grd <- expand.grid(
 
 point_vals_bot <- range_filter%>%
             group_by(LAT)%>%
-            summarise(DEPTH = max(DEPTH, na.rm = TRUE))
+            summarise(DEPTH = max(DEPTH, na.rm = TRUE))%>%
+            arrange(desc(LAT))
 
 point_vals_top <- range_filter%>%
           group_by(LAT)%>%
-          summarise(DEPTH = min(DEPTH, na.rm = TRUE))
+          summarise(DEPTH = min(DEPTH, na.rm = TRUE))%>%
+          arrange(LAT)
 
-point_vals <- bind_rows(point_vals_bot,point_vals_top)%>%
-  arrange(LAT,DEPTH)
+
 
 
 in_poly <- data.frame(in_poly = sp::point.in.polygon(idw_grd$LAT, idw_grd$DEPTH,
-                             point_vals$LAT, point_vals$DEPTH))
+                             c(point_vals_top$LAT,point_vals_bot$LAT),
+                             c(point_vals_top$DEPTH,point_vals_bot$DEPTH)))
 
 idw_grd <- idw_grd %>% bind_cols(in_poly)
 
@@ -61,4 +63,9 @@ idw_grd <- idw_grd %>% bind_cols(idw_mod)
 ggplot()+
   geom_raster(aes(LAT, -(DEPTH), fill = Z), data = idw_grd)+
   geom_ribbon(aes(x = LAT, ymin = -(max(DEPTH_BOTTOM, na.rm = TRUE)+10),
-                  ymax = -(DEPTH_BOTTOM)), data = range_filter)
+                  ymax = -(DEPTH_BOTTOM)), data = range_filter)+
+  #geom_point(aes(LAT, -(DEPTH)), shape = 0, data = range_filter)+
+  scale_fill_gradientn(colors = c("#0B222E","#01335C","#2D3B87","#564784",
+                                  "#765383","#985D80","#BF6574","#E2705C","#F68842",
+                                   "#FAAC3F","#F4D250","#E5FA6A"))+
+  theme_bw()
